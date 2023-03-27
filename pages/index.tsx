75d7cec67931b685d2ee41d52714d39cdab2768b
lohit244 <lohit244@gmail.com>
2023-03-27 20:44:13 +0530

    add colors to indicate the depth of subdirectory

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,27 @@ type FolderNode = {
   type: "folder",
   name: string,
   children: (FolderNode | FileNode)[],
+  depth: number,
 }
 type FileNode = {
   type: "file",
   name: string,
   linkPath: string
+  depth: number,
+}
+const depthColorMap:{[key:number]:string} = {
+  0: "text-white border-white",
+  1: "text-blue-300 border-blue-300",
+  2: "text-green-300 border-green-300",
+  3: "text-yellow-300 border-yellow-300",
+  4: "text-red-300 border-red-300",
+  5: "text-purple-300 border-purple-300",
+  6: "text-pink-300 border-pink-300",
+  7: "text-gray-300 border-gray-300",
+  8: "text-indigo-300 border-indigo-300",
+  9: "text-orange-300 border-orange-300",
+  10: "text-teal-300 border-teal-300",
+  // hopefully you don't have more than 10 subfolders
 }
 
 type FileTreeNode = FolderNode | FileNode;
@@ -19,7 +35,7 @@ const Card = (props: { file: FileNode }) => {
     <>
       <a
         href={props.file.linkPath.replaceAll("[", "%5B").replaceAll("]", "%5D")}
-        className="border border-white px-4 py-2 my-1 break-words rounded-md lg:hover:border-2 transition-all"
+        className={`${props.file.depth>0 && props.file.depth<12 && depthColorMap[props.file.depth-1]} border border-white px-4 py-2 my-1 break-words rounded-md lg:hover:border-2 transition-all`}
       >
         {props.file.name}
       </a>
@@ -33,7 +49,7 @@ const Subfolder = (props: { folder: FolderNode }) => {
   return (
     <div className="ml-4">
       <h1
-        className={`text-2xl ${vis ? "font-bold" : "font-light"
+        className={`text-2xl ${folder.depth<11 && depthColorMap[folder.depth]} ${vis ? "font-bold" : "font-light"
           } my-4 max-w-[20ch] sm:max-w-[30ch] md:max-w-[80%] break-words w-max cursor-pointer lg:hover:tracking-wide transition-all`}
         onClick={() => {
           setVis(!vis);
@@ -76,7 +92,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
 
 
-  const dfs = (folder: string, name: string): FileTreeNode => {
+  const dfs = (folder: string, name: string, depth: number): FileTreeNode => {
     if (!fs.existsSync(folder)) {
       return {
         type: "file",
@@ -88,12 +104,13 @@ export const getStaticProps: GetStaticProps = async () => {
       const subFiles = fs.readdirSync(folder);
       const subTree: FileTreeNode[] = [];
       subFiles.forEach((f: string) => {
-        subTree.push(dfs(folder + "/" + f, f));
+        subTree.push(dfs(folder + "/" + f, f, depth + 1));
       })
       const newNode: FolderNode = {
         type: "folder",
         name: name,
         children: subTree,
+        depth: depth,
       }
       return newNode;
     } else {
@@ -101,12 +118,13 @@ export const getStaticProps: GetStaticProps = async () => {
         type: "file",
         name: name,
         linkPath: folder.slice(8),
+        depth: depth,
       }
       return newNode;
     }
   };
 
-  const tree = dfs("./public", "root");
+  const tree = dfs("./public", "root", 0);
   return {
     props: {
       tree
