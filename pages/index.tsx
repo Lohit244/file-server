ef5e96728175aa7e084b5fdaaa6528070c46b4f8
lohit244 <lohit244@gmail.com>
2023-03-27 20:33:05 +0530

    improve to map the file data to an actual Tree Data structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,100 +1,115 @@
 import { GetStaticProps } from "next";
 import { useState } from "react";
 
-export default function Home(props: { paths: string[] }) {
-  const structureddata: { [key: string]: string[] } = {};
-  const m = new Set<string>();
-  props.paths.forEach((f) => {
-    if (f.indexOf("/") != -1) {
-      const index = f.search("/");
-      if (structureddata[f.slice(0, index)]) {
-        structureddata[f.slice(0, index)].push(f);
-      } else {
-        structureddata[f.slice(0, index)] = [];
-        structureddata[f.slice(0, index)].push(f);
-      }
-      m.add(f.split("/")[0]);
-    } else {
-      if (structureddata["root"]) {
-        structureddata["root"].push(f);
-      } else {
-        structureddata["root"] = [];
-        structureddata["root"].push(f);
-        m.add("root");
-      }
-    }
-  });
-  const marr = Array.from(m);
+type FolderNode = {
+  type: "folder",
+  name: string,
+  children: (FolderNode | FileNode)[],
+}
+type FileNode = {
+  type: "file",
+  name: string,
+  linkPath: string
+}
 
-  const Card = (props: { file: string }) => {
-    return (
-      <>
-        <a
-          href={props.file.replaceAll("[", "%5B").replaceAll("]", "%5D")}
-          className="border border-white px-4 py-2 my-1 break-words rounded-md lg:hover:border-2 transition-all"
-        >
-          {props.file}
-        </a>
-      </>
-    );
-  };
+type FileTreeNode = FolderNode | FileNode;
 
-  const Subfolder = (props: { folder: string }) => {
-    const folder = props.folder;
-    const [vis, setVis] = useState(false);
-    return (
-      <>
-        <h1
-          className={`text-2xl ${
-            vis ? "font-bold" : "font-light"
+const Card = (props: { file: FileNode }) => {
+  return (
+    <>
+      <a
+        href={props.file.linkPath.replaceAll("[", "%5B").replaceAll("]", "%5D")}
+        className="border border-white px-4 py-2 my-1 break-words rounded-md lg:hover:border-2 transition-all"
+      >
+        {props.file.name}
+      </a>
+    </>
+  );
+};
+
+const Subfolder = (props: { folder: FolderNode }) => {
+  const folder = props.folder;
+  const [vis, setVis] = useState(false);
+  return (
+    <div className="ml-4">
+      <h1
+        className={`text-2xl ${vis ? "font-bold" : "font-light"
           } my-4 max-w-[20ch] sm:max-w-[30ch] md:max-w-[80%] break-words w-max cursor-pointer lg:hover:tracking-wide transition-all`}
-          onClick={() => {
-            setVis(!vis);
-          }}
-        >
-          {folder} {vis ? "←" : "→"}
-        </h1>
-        <div className={`flex flex-col ${vis ? "" : "hidden"}`}>
-          {structureddata[folder].map((f) => {
-            return <Card file={f} key={f} />;
-          })}
-        </div>
-      </>
-    );
-  };
+        onClick={() => {
+          setVis(!vis);
+        }}
+      >
+        {folder.name} {vis ? "←" : "→"}
+      </h1>
+      <div className={`flex flex-col ${vis ? "" : "hidden"}`}>
+        {folder.children.map((f) => {
+          if (f.type == "folder") {
+            return <Subfolder folder={f} key={f.name} />;
+          }
+          return <Card file={f} key={f.linkPath} />;
+        })}
+      </div>
+    </div>
+  );
+};
+export default function Home(props: { tree: FileTreeNode }) {
+
 
   return (
     <div className="flex flex-col m-4">
       <h1 className="text-4xl font-black mt-4 mb-8">
         Welcome to The File Server
       </h1>
-      {marr.map((folder) => (
-        <Subfolder folder={folder} key={folder} />
-      ))}
+      {props.tree.type == "folder" ? (
+        <Subfolder folder={props.tree} key={props.tree.name} />
+      ) : (
+        <Card file={props.tree} key={props.tree.linkPath} />
+      )
+      }
+
     </div>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const fs = require("fs");
-  const files: string[] = [];
-  const dfs = (folder: string) => {
-    // console.log("ran @", folder);
+
+
+
+  const dfs = (folder: string, name: string): FileTreeNode => {
+    if (!fs.existsSync(folder)) {
+      return {
+        type: "file",
+        name: "404",
+        linkPath: "./public"
+      } as FileNode;
+    }
     if (fs.statSync(folder).isDirectory()) {
-      const t = fs.readdirSync(folder);
-      t.forEach((f: string) => {
-        dfs(folder + "/" + f);
-        // console.log("found ", f);
-      });
+      const subFiles = fs.readdirSync(folder);
+      const subTree: FileTreeNode[] = [];
+      subFiles.forEach((f: string) => {
+        subTree.push(dfs(folder + "/" + f, f));
+      })
+      const newNode: FolderNode = {
+        type: "folder",
+        name: name,
+        children: subTree,
+      }
+      return newNode;
     } else {
-      files.push(folder.slice(9));
+      const newNode: FileNode = {
+        type: "file",
+        name: name,
+        linkPath: folder.slice(8),
+      }
+      return newNode;
     }
   };
-  dfs("./public");
+
+  const tree = dfs("./public", "root");
   return {
     props: {
-      paths: files,
-    },
-    revalidate: 80,
-  };
+      tree
+    }
+  }
 };
